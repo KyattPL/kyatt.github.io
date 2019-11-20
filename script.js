0237627ce17f9638613906aa2f119fdea7be5aa7
Kajetan Pynka <siema1234@gazeta.pl>
2019-11-20 21:32:55 +0100

    Added handling of localStorage

--- script.js
+++ script.js
@@ -14,9 +14,43 @@ function addToList() {
     newItem.appendChild(newItemContent);
     newItem.appendChild(removeButton);
     list.appendChild(newItem);
+
+    saveLocalStorage();
 }
 
 function removeFromList(id) {
     let element = document.getElementById("li" + id);
     element.remove();
+
+    saveLocalStorage();
+}
+
+function saveLocalStorage() {
+    let numOfItems = document.getElementsByTagName("li").length;
+    window.localStorage.clear();
+
+    for (let i = 0; i < numOfItems; i++) {
+        let item = document.getElementsByTagName("li")[i].childNodes[0].textContent;
+        window.localStorage.setItem("li" + (i + 1), item);
+    }
+}
+
+function retrieveLocalStorage() {
+    let numOfItems = window.localStorage.length;
+    let list = document.getElementById("toDoList");
+    console.log(list)
+    for (let i = 0; i < numOfItems; i++) {
+        let itemText = window.localStorage.getItem("li" + (i + 1));
+        let textNode = document.createTextNode(itemText);
+        let itemNode = document.createElement("li");
+        itemNode.appendChild(textNode);
+
+        let removeButton = document.createElement("button");
+        removeButton.textContent = "Remove from list";
+        itemNode.id = "li" + i;
+        removeButton.id = i;
+        removeButton.onclick = function () { removeFromList(removeButton.id) };
+        itemNode.appendChild(removeButton);
+        list.appendChild(itemNode);
+    }
 }
